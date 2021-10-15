c179006fc856e270fbb08e580febe80f26d559e8
BBergsJ <goldlike888@gmail.com>
2021-10-15 23:55:36 +0300

    Лямбды и шаблон wrapper

--- src/main/java/ru/job4j/todo/store/HbmStore.java
+++ src/main/java/ru/job4j/todo/store/HbmStore.java
@@ -1,6 +1,7 @@
 package ru.job4j.todo.store;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todo.model.Item;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
     private static final StandardServiceRegistry REGISTRY = new StandardServiceRegistryBuilder().configure().build();
@@ -29,68 +31,62 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public Item create(Item item) {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> session.save(item));
         return item;
     }
 
     @Override
     public void update(int id) {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item i set i.done = case i.done when true then false else true end where i.id = :iId")
-        .setParameter("iId", id)
-        .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> session.createQuery("update Item i "
+                + "set i.done = case i.done when true then false else true end"
+                + " where i.id = :iId").setParameter("iId", id).executeUpdate()
+        );
     }
 
     @Override
     public void delete(Integer id) {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        Item item = new Item();
-        item.setId(id);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> {
+                Item item = session.load(Item.class, id);
+                session.delete(item);
+                return item;
+            }
+        );
     }
 
     @Override
     public List<Item> findAll() {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        List rsl = session.createQuery("from ru.job4j.todo.model.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return rsl;
+        return this.tx(session -> session.createQuery("from ru.job4j.todo.model.Item").list());
     }
 
     @Override
     public List<Item> findByFlagFalse() {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        List rsl = session.createQuery("from ru.job4j.todo.model.Item where done = :false")
-                .setParameter("false", false).list();
-        session.getTransaction().commit();
-        session.close();
-        return rsl;
+        return this.tx(session -> session.createQuery("from ru.job4j.todo.model.Item where done = :false")
+                    .setParameter("false", false).list()
+        );
     }
 
     @Override
     public Item findById(int id) {
-        Session session = SF.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(session -> session.get(Item.class, id));
     }
 
     @Override
     public void close() throws Exception {
         StandardServiceRegistryBuilder.destroy(REGISTRY);
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = SF.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
